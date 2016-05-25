dad0136a52eb8ffa34cb79d9310a1762f631d466
Adam Colton <adamcolton@gmail.com>
2016-05-24 22:13:35 -0400

    updateing Ajax module

--- ajax.js
+++ ajax.js
@@ -1,4 +1,18 @@
 Lapiz.Module("Ajax", ["Collections", "Events"], function($L){
+  var self = $L.Namespace();
+  $L.set($L, "Ajax", self.namespace);
+  $L.set(self.namespace, "on", $L.Map());
+
+  var _readystatechange = $L.Event.linkProperty(self.namespace.on, "readystatechange");
+  var _status = [
+    $L.Event.linkProperty(self.namespace.on, "status0"),
+    $L.Event.linkProperty(self.namespace.on, "status100"),
+    $L.Event.linkProperty(self.namespace.on, "status200"),
+    $L.Event.linkProperty(self.namespace.on, "status300"),
+    $L.Event.linkProperty(self.namespace.on, "status400"),
+    $L.Event.linkProperty(self.namespace.on, "status500"),
+  ];
+
   var _getXmlHttp;
   if (window.XMLHttpRequest){
     _getXmlHttp = function(){
@@ -21,21 +35,34 @@ Lapiz.Module("Ajax", ["Collections", "Events"], function($L){
   // Todo, see if there's an event passed in by onreadystatechange
   function _rsChng(x, callback){
     return function(){
-      if (x.readyState==4 && x.status==200){
-        callback(x);
+      _readystatechange.fire(x);
+      if (x.readyState === 4){
+        _status[Math.floor(x.status/100)].fire(x)
+        if (x.status === 200){
+          callback(x);
+        }
       }
     }
   }
+
+  var _headers = $L.Map();
 
   function request(type, url, urlData, rawData, callback, headers){
     var x = _getXmlHttp();
     if (urlData !== undefined){
       url += (url.indexOf("?") === -1) ? "?" : "&"
-      url += _encodeData(data);
+      if ($L.typeCheck.string(urlData)){
+        url += urlData;  
+      } else {
+        url += _encodeData(urlData);
+      }
     }
     if ($L.typeCheck.func(callback)){
       x.onreadystatechange = _rsChng(x, callback);
     }
+    $L.each(_headers, function(val, key){
+      x.setRequestHeader(key, val);
+    });
     if (headers !== undefined){
       $L.each(headers, function(val, key){
         x.setRequestHeader(key, val);
@@ -43,20 +70,39 @@ Lapiz.Module("Ajax", ["Collections", "Events"], function($L){
     }
     x.open(type, url, true);
     x.send(rawData);
+    return x;
   }
+  self.meth(request);
 
-  $L.Ajax = {
-    Request: request,
-    get: function(url, data, callback){
-      request("GET", url, data, undefined, callback);
-    },
-    post: function(url, data, callback, headers){
-      request("POST", url, data, undefined, callback);
-    },
-    json: function(url, data, callback, headers){
-      request("POST", url, data, undefined, function(x){
-        callback(JSON.parse(x.responseText));
-      });
+  self.meth(function get(url, data, callback, headers){
+    if ($L.typeCheck.func(data)){
+      callback = data;
+      data = undefined;
     }
-  };
+    return request("GET", url, data, undefined, callback, headers);
+  });
+
+  self.meth(function post(url, data, callback, headers){
+    if ($L.typeCheck.func(data)){
+      callback = data;
+      data = undefined;
+    }
+    return request("POST", url, data, undefined, callback, headers);
+  });
+
+  self.meth(function json(url, data, callback, headers){
+    return request("POST", url, data, undefined, function(x){
+      callback(JSON.parse(x.responseText));
+    }, headers);
+  });
+
+  self.meth(function header(key, val){
+    if (val === undefined){
+      return _headers[key];
+    } else if (val === null){
+      delete _headers[key];
+    } else {
+      _headers[key] = val;
+    }
+  });
 });
